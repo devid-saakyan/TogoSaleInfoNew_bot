b487496541e2a643507a4e0dd6b1736a16d21f99
devid-saakyan <59139074+devid-saakyan@users.noreply.github.com>
2021-11-15 04:51:58 +0300

    sbc

--- set_bot_commands.py
+++ set_bot_commands.py
@@ -3,5 +3,5 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands([
         types.BotCommand("start", "Запустить бота"),
-        types.BotCommand("menu", "Написать оперотору"),
+        types.BotCommand("menu", "Меню"),
     ])
