557ac49623e468041ca1353b24368c174aec2c3b
devid-saakyan <59139074+devid-saakyan@users.noreply.github.com>
2021-11-15 04:30:55 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,128 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher.storage import FSMContext
+from set_bot_commands import set_default_commands
+from dotenv import load_dotenv
+import os
+from time import sleep
+from keyboard import *
+
+
+load_dotenv()
+TOKEN = os.environ.get("TOKEN")
+
+bot = Bot(token = TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+async def on_startup(dp):
+    await set_default_commands(dp)
+
+
+class States(StatesGroup):
+    state1 = State()
+    state2 = State()
+    sklad = State()
+    ofis_s = State()
+    admin = State()
+    vajn_infa = State()
+    otdel_prodaj = State()
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer('Привет👋\nЯ ваш виртуальный помощник по освоению материала в нашей компании')
+    sleep(2)
+    await message.answer('Ах, да... Забыл представиться, я - бот Пётр, к вашим услугам')
+    sleep(2)
+    await message.answer("Чтобы мы продолжили дальнешее общение, вам необходимо заполнить форму по ссылке ниже\nПрошу, после завершения, нажать на кнопку 'Завершено'")
+
+    await message.answer('https://docs.google.com/forms/d/e/1FAIpQLSeuziM3rTD0Cwj2o_YkD5Vq67AbwE65Z80ZvV2evciSvCBsrw/viewform', reply_markup=keyboard1())
+
+@dp.message_handler(commands=['menu'])
+async def menu(message:types.Message):
+    await message.answer('Выберите пункт из меню: ', reply_markup=keyboard3())
+    await States.state2.set()
+
+@dp.callback_query_handler(lambda c: c.data == 'finish')
+async def process_after_finish(callback_query: types.CallbackQuery):
+    await bot.delete_message(callback_query.from_user.id, callback_query.message.message_id)
+    await bot.send_message(callback_query.from_user.id, text = 'Отлично! Теперь введите пароль, чтобы вы могли пользоваться мной!')
+
+@dp.message_handler(content_types=['text'])
+async def message(message:types.Message):
+    if message.text == '11111':
+        await message.answer('Открываю доступ...')
+        await message.answer('На какую должность вы вступаете:', reply_markup = keyboard2())
+        await States.state1.set()
+    else:
+        await message.answer('Неверный пароль, попробуйте ещё раз')
+
+@dp.message_handler(state = States.state1)
+async def state1(message: types.Message):
+    await message.answer('Хорошо, выберите пункт из меню', reply_markup=keyboard3())
+    await States.state2.set()
+
+@dp.message_handler(state = States.state2)
+async def state2(message: types.Message):
+    if message.text == 'Склад':
+        await message.answer('Вы выбрали пункт Склад, выберите направление из меню', reply_markup=keyboard_sklad())
+        await States.sklad.set()
+    elif message.text == 'Офис-собственника':
+        await message.answer('Вы выбрали пункт Офис-собственника, выберите направление из меню', reply_markup=keyboard_ofis())
+        await States.ofis_s.set()
+    elif message.text == 'Администарция':
+        await message.answer('Вы выбрали пункт Администарция, выберите направление из меню', reply_markup=keyboard_admin())
+        await States.admin.set()
+    elif message.text == 'Важная информация':
+        await message.answer('Вы выбрали пункт Важная информация, выберите направление из меню', reply_markup=keyboard_vajn_infa())
+        await States.vajn_infa.set()
+    elif message.text == 'Отдел продаж':
+        await message.answer('Вы выбрали пункт Отдел продаж, выберите направление из меню', reply_markup=keyboard_op())
+        await States.otdel_prodaj.set()
+
+@dp.message_handler(state = States.sklad)
+async def state_sklad(message: types.Message, state: FSMContext):
+    if message.text == '⬅ Назад':
+        await message.answer('Выберите пункт из меню:', reply_markup=keyboard3())
+        await States.state2.set()
+        return True
+    await message.answer('Пока пусто')
+    await state.reset_state()
+
+@dp.message_handler(state = States.ofis_s)
+async def state_sklad(message: types.Message, state: FSMContext):
+    if message.text == '⬅ Назад':
+        await message.answer('Выберите пункт из меню:', reply_markup=keyboard3())
+        await States.state2.set()
+        return True
+    await message.answer('Пока пусто')
+    await state.reset_state()
+
+@dp.message_handler(state = States.admin)
+async def state_sklad(message: types.Message, state: FSMContext):
+    if message.text == '⬅ Назад':
+        await message.answer('Выберите пункт из меню:', reply_markup=keyboard3())
+        await States.state2.set()
+        return True
+    await message.answer('Пока пусто')
+    await state.reset_state()
+
+@dp.message_handler(state = States.otdel_prodaj)
+async def state_sklad(message: types.Message, state: FSMContext):
+    if message.text == '⬅ Назад':
+        await message.answer('Выберите пункт из меню:', reply_markup=keyboard3())
+        await States.state2.set()
+        return True
+    await message.answer('Пока пусто')
+    await state.reset_state()
+
+@dp.message_handler(state = States.vajn_infa)
+async def state_vajn_infa(message: types.Message, state: FSMContext):
+    if message.text == '⬅ Назад':
+        await message.answer('Выберите пункт из меню:', reply_markup=keyboard3())
+        await States.state2.set()
+        return True
+    await message.answer('https://docs.google.com/document/d/1zdNjI7w92XhQyV1QZNbTMSxZaEV_dLz_kq8YTBPZWx8/edit')
+    await message.answer('https://docs.google.com/document/d/1AZsS7gbPooiElsF_kThMiNM27EcTvp50to4YcOKXWuY/edit')
+    await state.reset_state()
+executor.start_polling(dp, on_startup=on_startup)
---
+++ keyboard.py
@@ -0,0 +1,101 @@
+from aiogram import types
+
+def keyboard1():
+    markup = types.InlineKeyboardMarkup(resize_keyboard=True, selective=True)
+    button_1 = types.InlineKeyboardButton('Завершено', callback_data='finish')
+    markup.add(button_1)
+    return markup
+
+def keyboard2():
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    button1 = types.KeyboardButton('Управляющий')
+    button2 = types.KeyboardButton('HR')
+    button3 = types.KeyboardButton('Начальник склада')
+    button4 = types.KeyboardButton('IT-директор')
+    button5 = types.KeyboardButton('Бухгалтер')
+    button6 = types.KeyboardButton('Офис-менеджер')
+    button7 = types.KeyboardButton('Ассистент руководителя')
+    button8 = types.KeyboardButton('Ассистент управляющего')
+    markup.add(button1, button2, button3, button4, button5, button6, button7, button8)
+    return markup
+
+def keyboard3():
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    button1 = types.KeyboardButton('Склад')
+    button2 = types.KeyboardButton('Офис-собственника')
+    button3 = types.KeyboardButton('Администарция')
+    button4 = types.KeyboardButton('Важная информация')
+    button5 = types.KeyboardButton('Отдел продаж')
+    markup.add(button1, button2, button3, button4, button5)
+    return markup
+
+def keyboard_sklad():
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    button1 = types.KeyboardButton('Обеспечение жизнедеятельности склада')
+    button2 = types.KeyboardButton('Распределение магазинов и ИП')
+    button3 = types.KeyboardButton('Регламент работы сотрудников склада')
+    button4 = types.KeyboardButton('Начальник склада')
+    button5 = types.KeyboardButton('Закупка')
+    button_back = types.KeyboardButton('⬅ Назад')
+    sp = [button1, button2, button3, button4, button5]
+    for i in sp:
+        markup.row(i)
+    markup.row(button_back)
+    return markup
+
+def keyboard_ofis():
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    button1 = types.KeyboardButton('Юридическая безопасность')
+    button2 = types.KeyboardButton('Организационная политика')
+    button3 = types.KeyboardButton('Финаносовая система')
+    button4 = types.KeyboardButton('Технологии')
+    button5 = types.KeyboardButton('Управление директором')
+    button6 = types.KeyboardButton('Управление ценным имуществом')
+    button7 = types.KeyboardButton('Построение компании')
+    sp = [button1, button2, button3, button4, button5, button6, button7]
+    button_back = types.KeyboardButton('⬅ Назад')
+    for i in sp:
+        markup.row(i)
+    markup.row(button_back)
+    return markup
+
+def keyboard_admin():
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    button1 = types.KeyboardButton('Должностные инструкции')
+    button2 = types.KeyboardButton('Логистика')
+    button3 = types.KeyboardButton('IT-отдел')
+    button4 = types.KeyboardButton('HR-отдел')
+    button_back = types.KeyboardButton('⬅ Назад')
+    markup.add(button1, button2, button3, button4)
+    markup.row(button_back)
+    return markup
+
+def keyboard_vajn_infa():
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    button1 = types.KeyboardButton('Шаблон письма для регистрации ТЗ')
+    button2 = types.KeyboardButton('Дорога до офиса и склада')
+    button3 = types.KeyboardButton('Контакты сотрудников')
+    button4 = types.KeyboardButton('Увольнение')
+    button5 = types.KeyboardButton('Чаты компании')
+    button6 = types.KeyboardButton('Миссия и цели компании')
+    button7 = types.KeyboardButton('Протоколы планерок')
+    button8 = types.KeyboardButton('Шаблон для постановки задач')
+    button9 = types.KeyboardButton('Список печатей и их наличие на складе и в офисе')
+    button10 = types.KeyboardButton('Закупка канцелярии в офис')
+    button11 = types.KeyboardButton('Инструкция по обращению с пропусками')
+    sp = [button2, button3, button4, button5, button6, button7, button8, button10, button11]
+    button_back = types.KeyboardButton('⬅ Назад')
+    for i in sp:
+        markup.row(i)
+    markup.row(button_back)
+    return markup
+
+def keyboard_op():
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=False, resize_keyboard=True)
+    button1 = types.KeyboardButton('Распределение магазинов и ИП')
+    button2 = types.KeyboardButton('Инвентаризация склада')
+    button3 = types.KeyboardButton('Работа с Ozon и WB')
+    button_back = types.KeyboardButton('⬅ Назад')
+    markup.add(button1, button2, button3)
+    markup.row(button_back)
+    return markup
